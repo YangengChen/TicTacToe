faa03535377f3371767fbc39b8e7c0663f604dd1
Sajid Kamal <sajidkamal@Sajids-MacBook-Air.local>
2017-05-03 22:14:03 -0400

    added some server logic

--- GameServer.py
+++ GameServer.py
@@ -1,4 +1,6 @@
 from threading import Thread
+import Game
+import Player
 import socketserver
 import sys
 
@@ -8,6 +10,15 @@ mode = ''
 games = []
 available_players = []
 busy_players = []
+
+help_menu = 'login [name]       Log into TicTacToe with username <name>\n' +
+            'place [1-9]        Place piece on box <1-9> during your turn\n' +
+            'exit               Exits TicTacToe ending current game\n' + 
+            'games              Display list of ongoing games and info\n' +
+            'who                Display list of available players\n' +
+            'play [name]        Challenge player <name>\n' +
+            'observe [gameid]   Observe game <gameid>\n' +
+            'unobserve [gameid] Stop observing game <gameid>'
 
 class GameTCPHandler(socketserver.BaseRequestHandler):
 
@@ -19,10 +30,50 @@ class GameTCPHandler(socketserver.BaseRequestHandler):
             self.request.sendall(response)
 
     def handle_command(comm):
-        if (comm == 'games'):
-        elif (comm == 'who'):
-        elif (comm == 'play'):
+    	commands = comm.split(" ")
+    	if(commands[0] == 'login'):
+    		create_player(commands[1])
+
+        if (commands[0] == 'games'):
+        	return print_games()
+
+        elif (commands[0] == 'who'):
+        	avail = ', '.join(names)
+        	return avail;
+
+        elif (commands[0] == 'play'):
+        	#implement play
         else:
+        	return help()
+
+    def print_games():
+    	gameString = '';
+    	for game in games:
+    		gameString += 'Game Id: '+
+    		 				'\tPlayer 1: '+game.player1.name+'\n'
+    		 				'\tPlayer 2: '+game.player2.name+'\n'
+
+    	return gameString
+
+    def create_player(name):
+    	player = Player(name)
+    	available_players.append(player)
+
+
+	def help():
+    	return print(help_menu)
+
+    def play(other_player):
+    	if any(player.name == other_player for player in available_players)
+    		player2 = next((player for player in available_players if player.name == other_player), None)
+    		new_game = Game
+
+
+    	else:
+    		return 'Player not available to play'
+
+
+
 
 
 def __exit(code):
