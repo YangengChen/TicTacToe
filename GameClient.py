3ea6ef6469f4550c41fc54bdd24870e44cd98a57
YangengChen <yangengchen@gmail.com>
2017-05-03 23:13:00 -0400

    Update GameClient.py

--- GameClient.py
+++ GameClient.py
@@ -1,5 +1,7 @@
 import GameBoard
 import Player
+import socket
+import sys
 
 help_menu = 'login [name]       Log into TicTacToe with username <name>\n' +
             'place [1-9]        Place piece on box <1-9> during your turn\n' +
@@ -10,9 +12,86 @@ help_menu = 'login [name]       Log into TicTacToe with username <name>\n' +
             'observe [gameid]   Observe game <gameid>\n' +
             'unobserve [gameid] Stop observing game <gameid>'
 
+host = sys.argv[1]
+port = int(sys.argv[2])
 
-def help():
+clientSocket = socket(AF_INET, SOCK_STREAM)
+clientSocket.connect(host, port)
+
+while(1):
+    command = raw_input("Enter a command: ")
+    check_command(command)
+
+def print_help():
     return print(help_menu)
 
+def login(name):
+    clientSocket.send("login " + name)
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
 
-def 
+def place(pos):
+    clientSocket.send("place " + pos)
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
+    
+def games():
+    clientSocket.send("games")
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
+
+def who():
+    clientSocket.send("who")
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
+
+def play(name):
+    clientSocket.send("play " + name)
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
+
+def observe(gameID):
+    clientSocket.send("observe " + gameID)
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
+    
+def unobserve(gameID):
+    clientSocket.send("unobserve " + gameID)
+    response = clientSocket.recv(1024)
+    print("Server: "+response)
+
+def exit_server():
+    clientSocket.send("exit")
+    response = clientSocket.recv(1024)
+    print("Server: "+response)    
+    exit()
+    
+def check_command(command):
+    command = command.split()
+    if len(command) == 1:
+        if command[0] == "help":
+            print_help()
+        elif command[0] == "games":
+            games()
+        elif command[0] == "who":
+            who()
+        elif command[0] == "exit"
+            exit_server()
+        else:
+            print("Unsupported command")
+    elif len(command) == 2:
+        if command[0] == "login":
+            login(command[1])
+        elif command[0] == "place":
+            place(command[1])
+        elif command[0] == "play":
+            play(command[1])
+        elif command[0] == "observe":
+            observe(command[1])
+        elif command[0] == "unobserve":
+            unobserve(command[1])
+        else:
+            print("Unsupported command")
+    else:
+        print("Unsupported command") 
+                   
